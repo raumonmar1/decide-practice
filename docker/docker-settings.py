659fedfdcaf3349a0465d0f58a628d924b83f04a
raumonmar1 <72814383+raumonmar1@users.noreply.github.com>
2023-11-30 09:26:18 +0100

    Update docker-settings.py

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -3,9 +3,9 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD':'',
+        'NAME': 'decidedb2',
+        'USER': 'decideuser',
+        'PASSWORD':'decidepass123',
         'HOST': 'db',
         'PORT': 5432,
     }
@@ -15,7 +15,7 @@ STATIC_ROOT = '/app/static/'
 MEDIA_ROOT = '/app/static/media/'
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['http://10.5.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['http://10.5.0.1:8000', 'http://localhost:8000']
 
 # Modules in use, commented modules that you won't use
 MODULES = [
